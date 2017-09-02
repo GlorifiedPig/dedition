d82d8c05c5e19dbafd920ef0931507435f60517e
GlorifiedPig <killingpigs123@gmail.com>
2017-09-02 12:50:39 +0200

    import liro gamemode base

---
+++ gamemode/liro/config.lua
@@ -0,0 +1,48 @@
+-- Liro - liro/config.lua
+
+-- This is the main configuration file for the Liro gamemode base, it
+-- allows you to customize the boot of the gamemode.
+
+liro.config = {
+	-- Output basic module load information on gamemode initialization?
+	doModuleLoadMessages = true,
+
+	-- Enable the module system?
+	enableModules = true, 
+
+	-- Enable developer hooks for liro events?
+	enableDeveloperHooks = true,
+        
+	-- Enable AddNetworkString table?
+	enableNetworkStrings = true,
+
+	-- Register module file name
+	-- It's recommended to NOT change this (developer reasons only)
+	registerFileName = "registermodule.lua",
+
+	-- Include your network strings in here and they will be util.AddNetworkString'd before module initalization. (e.g. "networkString", "")
+	-- This will only work if enableNetworkStrings is set to true.
+	-- Modules may also contain network strings.
+	
+	-- Note that util.AddNetworkString only supports 2048 networkStrings.
+	-- http://wiki.garrysmod.com/page/util/AddNetworkString
+	networkStrings = {},
+
+	-- Disable any modules? (e.g. "moduleName", "")
+	disabledModuleNames = {},
+	
+	-- Blacklisted Files (stops files from loading) (e.g. "fileName", "")
+	-- This works recursively (within folders of the module)
+	globalBlacklistedFiles = {},
+
+	-- Global prefixes that define the enviroment that the file will be loaded in.
+	-- These prefixes are overidden if the module specific load prefixes set in it's registermodule.lua
+	-- Note: It's recommended not to change these, for if the module authors don't specify per module prefixes and expect 'sv_', 'cl_' and 'sh_' to be used
+	moduleLoadPrefixes = {
+		server = "sv_",
+		client = "cl_",
+		shared = "sh_"
+	}
+}
+
+-- See the facepunch thread for a list of developer hooks
---
+++ gamemode/liro/functions.lua
@@ -0,0 +1,44 @@
+-- Liro - liro/functions.lua
+
+-- liro.activateDeveloperHook()
+-- Calls a hook after checking if developer hooks are enabled
+function liro.activateDeveloperHook(name, arg)
+	if liro.enableDeveloperHooks then
+		if arg == nil or arg == "" then
+			hook.Call(name)
+		else
+			hook.Call(name, arg)
+		end
+	end
+end
+
+-- liro.isEmpty(variable)
+-- Checks if a variable is nil
+function liro.isEmpty(s)
+	return s == nil or s == ""
+end
+
+-- liro.moduleIntegrity(moduleData)
+-- Checks if provided module data is valid
+function liro.moduleIntegrity(md)
+	if liro.isEmpty(md.folderName) or liro.isEmpty(md.loadPriority) or liro.isEmpty(md.author) or liro.isEmpty(md.description) or liro.isEmpty(md.website) or liro.isEmpty(md.version) then
+		return false
+	end
+	return true
+end
+
+-- liro.isModuleLoaded(module)
+-- Returns if the provided module name/data is loaded
+function liro.isModuleLoaded(module)
+	if moduleName != "" then
+		if istable(module) then
+			if liro.moduleIntegrity(module) then
+				return liro.loadedModules[module.folderName]
+			else
+				return false	
+			end
+		else
+			return liro.loadedModules[moduleName]
+		end
+	end
+end
