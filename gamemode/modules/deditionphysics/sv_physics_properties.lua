453df4fce56054cd1a4e1828ac0e6823251b862f
GlorifiedPig <killingpigs123@gmail.com>
2017-09-25 15:57:23 +0200

    minor changes

--- gamemode/modules/deditionphysics/sv_physics_properties.lua
+++ gamemode/modules/deditionphysics/sv_physics_properties.lua
@@ -1,5 +1,11 @@
 
-function GetFallDamageDedition( ply, speed )
+local function SetSpeedsDedition( ply )
+	ply:SetWalkSpeed( 100 )
+	ply:SetRunSpeed( 300 )
+end
+hook.Add( "PlayerInitialSpawn", "SetSpeedsDedition", SetSpeedsDedition )
+
+local function GetFallDamageDedition( ply, speed )
 	return ( speed / 8 )
 end
 hook.Add( "GetFallDamage", "GetFallDamageDedition", GetFallDamageDedition )
